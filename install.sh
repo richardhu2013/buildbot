c350367c88a626b2d50d938dc20080033adcbf27
Richard Hu 1 <richardhu2013>
2017-02-05 20:53:07 +1100

    Change install.sh to install properly

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-sudo apt-get update
-sudo apt-get upgrade -y
+#!/bin/bash
+# sudo apt-get update
+# sudo apt-get upgrade -y
 sudo apt-get -y install git vim curl
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.4/install.sh | bash
 
@@ -8,6 +9,5 @@ nvm install v5
 nvm alias default v5
 npm install grunt-cli -g
 
-git clone https://github.com/richardhu2013/buildbot.git
-cd buildbot && npm install
+npm install
 
