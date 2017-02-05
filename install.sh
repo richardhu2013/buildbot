b5e76222ef2e5eaefe2a3e7da8bdc171341afe58
Richard Hu 1 <richardhu2013>
2017-02-05 20:40:06 +1100

    Add install.sh for easy setup

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get -y install git vim curl
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.4/install.sh | bash
+
+source ~/.nvm/nvm.sh
+nvm install v5
+nvm alias default v5
+npm install grunt-cli -g
+
+git clone https://github.com/richardhu2013/buildbot.git
+cd buildbot && npm install
+
